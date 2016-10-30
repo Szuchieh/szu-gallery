ee2278b957507d552b1eca73f7b1963f1c1e3884
Mikel Duffy <mduffy23@gmail.com>
2016-10-29 23:14:04 -0700

    Implements basic navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
-  $(".button-collapse").sideNav();
-  $(".brand-logo").on("click", function() {
-    $(".ava").hide();
-    $(".chime").hide();
+  $(window).trigger('hashchange'); // Need it?
+
+  $('.button-collapse').sideNav({closeOnClick: true});
+
+  $(window).on('hashchange', function(){
+    render(decodeURI(window.location.hash));
   });
+
+  function render(url) {
+    switch (url) {
+      case '#Home':
+        $('.Ava').hide();
+        $('.Chime').hide();
+        $('.About').hide();
+        $('.Homepage').show();
+        break;
+      case '#About':
+        $('.Ava').hide();
+        $('.Chime').hide();
+        $('.About').show();
+        $('.Homepage').hide();
+        break;
+      case '#Ava':
+        $('.Ava').show();
+        $('.Chime').hide();
+        $('.About').hide();
+        $('.Homepage').hide();
+        break;
+      case '#Chime':
+        $('.Ava').hide();
+        $('.Chime').show();
+        $('.About').hide();
+        $('.Homepage').hide();
+        break;
+      default:
+
+    }
+  }
 });
