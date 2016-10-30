b122521563b6a0cacf186d91ed7ed2910b146c0b
Mikel Duffy <mduffy23@gmail.com>
2016-10-29 22:15:40 -0700

    Adds favicon and work sample buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
-  // $(".work-sample").hover(function() {
-  //   $(this).animate({opacity: .5}, 150, function() {});
-  // });
-  // $(".work-sample").on("mouseleave", function() {
-  //   $(this).animate({opacity: 1}, 150, function() {});
-  // });
+  $(".brand-logo").on("click", function() {
+    $(".ava").hide();
+    $(".chime").hide();
+  });
 });
