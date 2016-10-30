683393f544846060494da3a46d3ea91904f4ddb2
Mikel Duffy <mduffy23@gmail.com>
2016-10-29 21:54:27 -0700

    Adds work sample sections

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+  // $(".work-sample").hover(function() {
+  //   $(this).animate({opacity: .5}, 150, function() {});
+  // });
+  // $(".work-sample").on("mouseleave", function() {
+  //   $(this).animate({opacity: 1}, 150, function() {});
+  // });
 });
